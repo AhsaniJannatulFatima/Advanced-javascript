19163a52689642787d4633717426a9a4857987c5
AhsaniJannatulFatima <ahsanisarwarsharmi@gmail.com>
2020-08-14 03:46:58 +0600

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+let pakhi;
+console.log(pakhi);
+function add(num1 ,num2){
+  console.log(num1 + num2);
+return
+}
+function add(num1 ,num2){
+   console.log(num1 , num2);
+}
+const result = add( 20);
+console.log(result);
+const premik = {name:"smart dude" , phone:45678};
+console.log(premik.gf);
+let fun =  undefined;
+console.log(fun);
+
+let ages = [ 2 ,3,5];
+console.log(ages[11]);
