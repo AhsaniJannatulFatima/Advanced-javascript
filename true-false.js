18310f57c63583dde3ab11e12460bd6059f459c3
AhsaniJannatulFatima <ahsanisarwarsharmi@gmail.com>
2020-08-13 23:17:33 +0600

    true/false

---
+++ true-false.js
@@ -0,0 +1,31 @@
+//0
+//""
+//undefined
+//null
+//NaN
+//truthy
+//'0', ' ' []
+
+const age = 0;
+if(age){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+const name = " ";
+if( name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+//let
+let color = "false";
+if(color){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
